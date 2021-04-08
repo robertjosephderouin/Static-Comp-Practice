e7a882b68c993b3f60346a7c9615ca5e35f138a2
robertjosephderouin <robertjosephderouin@gmail.com>
2021-04-08 11:06:46 -0700

    Add basic HTML and CSS layout

---
+++ src/main.js
@@ -0,0 +1,9 @@
+class Recipient {
+  constructor(name, date, url, playlist, attachments) {
+    this.name = name;
+    this.date = date;
+    this.url = url;
+    this.playlist = playlist;
+    this.attachments = attachments;
+  }
+}
